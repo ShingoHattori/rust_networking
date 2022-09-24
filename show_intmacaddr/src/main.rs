fb16dfc350b98093a238162d8a27990b4734e51a
Shingo Hattori <cely.ilus@gmail.com>
2022-09-25 00:52:45 +0900

    edit リファクタリングしてコメントを追加した

--- show_intmacaddr/src/main.rs
+++ show_intmacaddr/src/main.rs
@@ -1,19 +1,12 @@
 extern crate pnet;
  
 use pnet::datalink::{self, NetworkInterface};
-use pnet::datalink::Channel::Ethernet;
-use pnet::packet::ethernet::{EtherTypes, EthernetPacket, MutableEthernetPacket};
-use pnet::packet::ipv4::Ipv4Packet;
-use pnet::packet::tcp::TcpPacket;
-use pnet::packet::udp::UdpPacket;
-use pnet::packet::ip::IpNextHeaderProtocols;
-use pnet::packet::Packet;
 
 use std::env;
  
 
 fn main() {
-    // インターフェース名を引き数で取得
+    // インターフェース名を引き数で取得．
     let network_interface1 = env::args().nth(1).unwrap();
 
     let interfaces1 = datalink::interfaces();
@@ -23,7 +16,10 @@ fn main() {
     println!("Inteface1:{}",interface1.name);
 
     let mac = interface1.mac;
+
+    // interfaceがnullの場合はmac addrも当然nullになりうるので，macはswiftのoptionalみたいになる．なので，unwrapみたいなことをする．
     if let Some(mac) = mac {
-       println!("mac1:{:?}",mac);
+      // mac addr構造体をいい感じにstringに変換する．
+      println!("mac1:{:?}",mac);
     }
 }
